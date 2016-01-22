6309cad470c00b37ba950f99f752bfeb397b77cb
light-zhao <2568252796@qq.com>
2016-01-22 18:21:41 +0800

    0.0.1
    有只丧尸可以打几下

---
+++ Assets/script/effect/GameObjectSortLayerArrangement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameObjectSortLayerArrangement : MonoBehaviour {
+	public static GameObjectSortLayerArrangement instance;
+	public List<GameObject> inViewObjects=new List<GameObject>();
+	void Awake(){
+		instance=this;
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(inViewObjects.Count>0){
+			inViewObjects.Sort(delegate(GameObject a,GameObject b) {
+				return a.transform.position.y.CompareTo(b.transform.position.y)*-1;
+		});
+		}
+		for(int i=0;i<inViewObjects.Count;i++){
+			SortLayerMarker slm=inViewObjects[i].GetComponent<SortLayerMarker>();
+			if(slm){
+				slm.setSortedLayer("g"+i.ToString());
+			}
+		}
+	}
+	public void add(GameObject go){
+		if(inViewObjects.Contains(go))
+			return;
+		inViewObjects.Add(go);
+	}
+	public void remove(GameObject go){
+		inViewObjects.Remove(go);
+	}
+}
---
+++ Assets/script/effect/SpritesTwinkle.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpritesTwinkle : MonoBehaviour {
+	public string tag;
+	public bool twinkle=false;
+	int twinkleCount=0;
+	const int maxTwinkleCount=5;
+	bool showed=false;
+	public SpriteRenderer[] sprites;
+	void Awake(){
+		getSprites();
+		
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	public void getSprites(){
+		sprites=GetComponentsInChildren<SpriteRenderer>();
+	}
+	void Update(){
+		if(twinkle){
+			if(twinkleCount>=maxTwinkleCount){
+				twinkleCount=0;
+				setTagGroupVisible(showed);
+				showed=!showed;
+			}
+			twinkleCount++;
+		}
+	}
+	void setVisible(SpriteRenderer spr,bool visible){
+		Color c=spr.color;
+		if(visible){
+			c.a=1;
+		}else{
+			c.a=0;
+		}
+		spr.color=c;
+	}
+	void setTagGroupVisible(bool visible){
+		foreach(SpriteRenderer spr in sprites){
+			if(spr.tag==tag){
+				setVisible(spr,visible);
+			}
+		}
+	}
+//	IEnumerator twinkleStep(){
+//		while(true){
+//			setTagGroupVisible(showed);
+//			showed=!showed;
+//			yield return new WaitForSeconds(1.2f);
+//		}
+//	}
+	public void start(){
+		twinkle=true;
+	}
+	public void stop(){
+		twinkle=false;
+		twinkleCount=maxTwinkleCount;
+		setTagGroupVisible(true);
+	}
+}
