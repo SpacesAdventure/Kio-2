cd5c9b4dd12c76c353a2bea4ae06d00c947020f0
light-zhao <2568252796@qq.com>
2016-01-26 23:36:57 +0800

    0.0.3
    僵尸有ai寻路了，有散弹枪可以换了，然后木有然后了

---
+++ Assets/script/ai/ZombieMover.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using HutongGames.PlayMaker;
+using Pathfinding;
+
+public class ZombieMover : MonoBehaviour {
+	GameObject player;
+	public Vector3[] points;
+	Vector3 nextDestination;
+	Path path;
+	Seeker seeker;
+	// Use this for initialization
+	void Start () {
+		seeker=GetComponent<Seeker>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(player==null){
+			FsmVariables fvs= PlayMakerGlobals.Instance.Variables;
+			FsmGameObject fmgo=fvs.FindFsmGameObject("player");
+			if(fmgo!=null){
+				player=fmgo.Value;
+			}
+			return;
+		}
+		seeker.StartPath(transform.position,player.transform.position,OnPathComplete);
+		points=path.vectorPath.ToArray();
+		nextDestination=points[1];
+	}
+	public void OnPathComplete (Path p) {
+		path=p;
+	}
+	public Vector3 getClosesPoint(){
+		return nextDestination;
+	}
+	public int getPointsSize(){
+		return points.Length;
+	}
+}
