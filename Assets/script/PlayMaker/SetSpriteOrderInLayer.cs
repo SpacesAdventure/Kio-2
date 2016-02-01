702b3b71f226a376f8bb0ad31da65bd859e20eab
light-zhao <2568252796@qq.com>
2016-02-01 18:08:51 +0800

    0.0.7
    能通过按esc回到标题画面
    
    增加了开枪细节，注意子弹会跳到桌子上面这个显示bug
    
    昨日讲的都基本做好了
    
    卡桌子bug还没解决

---
+++ Assets/script/PlayMaker/SetSpriteOrderInLayer.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory("Sprite")]
+	[Tooltip("Set the sorting order in a SpriteRendered. Optionally set all SpriteRendered found on the gameobject Target.")]
+	public class SetSpriteOrderInLayer : FsmStateAction
+	{
+		[RequiredField]
+		public FsmOwnerDefault gameObject;
+
+		[RequiredField]
+		public FsmInt order;
+
+
+		public bool setAllSpritesInChildren;
+
+		public override void Reset()
+		{
+			gameObject = null;
+			order = null;
+			setAllSpritesInChildren = false;
+		}
+
+		public override string ErrorCheck ()
+		{
+			GameObject go = Fsm.GetOwnerDefaultTarget(gameObject);
+
+			if (go!=null && ! setAllSpritesInChildren && go.GetComponent<SpriteRenderer>() == null ){
+				return "Missing SpriteRenderer on GameObject";
+			}
+
+			return "";
+		}
+		public override void OnEnter()
+		{
+			DoSetLayerName();
+			Finish();
+		}
+
+		void DoSetLayerName()
+		{
+			GameObject go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (go == null) return;
+
+
+			if (setAllSpritesInChildren)
+			{
+				SpriteRenderer[] sprites = go.GetComponentsInChildren<SpriteRenderer> ();
+				foreach (SpriteRenderer _sprite in sprites) {
+					_sprite.sortingOrder = order.Value;
+				}
+			}else{
+				if (go.GetComponent<SpriteRenderer>() != null)go.GetComponent<SpriteRenderer>().sortingOrder  = order.Value;
+			}
+		}
+	}
+}
+
---
+++ Assets/script/PlayMaker/SpriteFlip.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory("sprite")]
+	[Tooltip("Flip a single Sprite on any GameObject. Object must have a Sprite Renderer.")]
+	public class FlipSprite : FsmStateAction
+	{
+
+		[RequiredField]
+		[CheckForComponent(typeof(SpriteRenderer))]
+		public FsmOwnerDefault gameObject;
+
+		public FsmBool resetOnExit;
+
+		private SpriteRenderer spriteRenderer;
+
+		public FsmBool x;
+		public FsmBool y;
+		bool _x;
+		bool _y;
+
+		public override void Reset()
+		{
+			gameObject = null;
+			resetOnExit = false;
+			x=null;
+			y=null;
+
+		}
+
+		public override void OnEnter()
+		{
+			GameObject go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (go == null) return;
+
+			spriteRenderer = go.GetComponent<SpriteRenderer>();
+
+			if (spriteRenderer == null)
+			{
+				LogError("SwapSingleSprite: Missing SpriteRenderer!");
+				return;
+			}
+			_x=spriteRenderer.flipX;
+			_y=spriteRenderer.flipY;
+
+
+			filp();
+
+			Finish();
+		}
+
+		public override void OnExit()
+		{
+			if (resetOnExit.Value)
+			{
+				spriteRenderer.flipX=_x;
+				spriteRenderer.flipY=_y;
+			}
+		}
+		void filp()
+		{
+			spriteRenderer.flipX=x.Value;
+			spriteRenderer.flipY=y.Value;
+		}
+
+
+
+	}
+}
