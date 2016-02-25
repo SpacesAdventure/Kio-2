71a5c34d42270d522d50a29e4a28f2327233e1e3
light-zhao <2568252796@qq.com>
2016-02-25 18:47:06 +0800

    0.0.9--

---
+++ Assets/script/PlayMaker/GameObjectIsActived.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Logic)]
+	[Tooltip("Tests if a GameObject Variable has a null value. E.g., If the FindGameObject action failed to find an object.")]
+	public class GameObjectIsActived : FsmStateAction
+	{
+		[RequiredField]
+		[UIHint(UIHint.Variable)]
+		[Tooltip("The GameObject variable to test.")]
+		public FsmGameObject gameObject;
+
+		[Tooltip("Event to send if the GamObject is null.")]
+		public FsmEvent isActived;
+
+		[Tooltip("Event to send if the GamObject is NOT null.")]
+		public FsmEvent isNotActived;
+
+		[UIHint(UIHint.Variable)]
+		[Tooltip("Store the result in a bool variable.")]
+		public FsmBool storeResult;
+
+		[Tooltip("Repeat every frame.")]
+		public bool everyFrame;
+
+		public override void Reset()
+		{
+			gameObject = null;
+			isActived = null;
+			isNotActived = null;
+			storeResult = null;
+			everyFrame = false;
+		}
+
+		public override void OnEnter()
+		{
+			DoIsGameObjectNull();
+
+			if (!everyFrame)
+			{
+				Finish();
+			}
+		}
+
+		public override void OnUpdate()
+		{
+			DoIsGameObjectNull();
+		}
+
+		void DoIsGameObjectNull()
+		{
+			var goIsNull = gameObject.Value.activeInHierarchy;
+
+			if (storeResult != null)
+			{
+				storeResult.Value = goIsNull;
+			}
+
+			Fsm.Event(goIsNull ? isActived : isNotActived);
+		}
+	}
+}
---
+++ Assets/script/PlayMaker/uGuiImageSetNativeSize.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory("uGui")]
+	[Tooltip("Sets the native size of a UGui Image component.")]
+	public class uGuiImageSetNativeSize : FsmStateAction
+	{
+		[RequiredField]
+		[CheckForComponent(typeof(UnityEngine.UI.Image))]
+		[Tooltip("The GameObject with the Image ui component.")]
+		public FsmOwnerDefault gameObject;
+
+//		[RequiredField]
+//		[Tooltip("The source sprite of the UGui Image component.")]
+//		[ObjectType(typeof(UnityEngine.Sprite))]
+//		public FsmObject sprite;
+
+		[Tooltip("Reset when exiting this state.")]
+		public FsmBool resetOnExit;
+
+		UnityEngine.UI.Image _image;
+//		UnityEngine.Sprite _originalSprite;
+
+
+		public override void Reset()
+		{
+			gameObject = null;
+			resetOnExit = false;
+		}
+
+		public override void OnEnter()
+		{
+			GameObject _go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (_go!=null)
+			{
+				_image = _go.GetComponent<UnityEngine.UI.Image>();
+			}
+
+			DoSetImageSourceValue();
+
+			Finish();
+		}
+
+		void DoSetImageSourceValue()
+		{
+			if (_image==null)
+			{
+				return;
+			}
+
+			if (resetOnExit.Value)
+			{
+//				_originalSprite = _image.sprite;
+			}
+
+			_image.SetNativeSize();
+		}
+
+		public override void OnExit()
+		{
+			if (_image==null)
+			{
+				return;
+			}
+
+			if (resetOnExit.Value)
+			{
+//				_image.sprite = _originalSprite;
+			}
+		}
+
+	}
+}
